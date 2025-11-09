723cbd025ba10d042e23a6be7cca8cd51c4917a8
ud2 <sjx233@qq.com>
2025-11-09 09:08:34 +0800

    reallocate by transferring

--- uint8_array_writer.ts
+++ uint8_array_writer.ts
@@ -1,22 +1,4 @@
-const { isFinite } = Number;
-const f64Buf = new Float64Array(1);
-const u64Buf = new BigUint64Array(f64Buf.buffer);
-
-/**
- * Returns the smallest integral power of two that is not smaller than `x`,
- * or `NaN` if `x` is `NaN`.
- */
-function bitCeil(x: number): number {
-  if (x <= 1) {
-    return 1;
-  }
-  if (!isFinite(x)) {
-    return x;
-  }
-  f64Buf[0] = x;
-  u64Buf[0] = ((u64Buf[0] - 1n) | 0xfffffffffffffn) + 1n;
-  return f64Buf[0];
-}
+const { max } = Math;
 
 function mightGrow(array: Uint8Array, originalLength: number): boolean {
   return "growable" in array.buffer &&
@@ -56,16 +38,12 @@ export class Uint8ArrayWriter {
       chunk = chunk.subarray(0, provided);
     }
     const buffered = buffer.length;
-    const storage = buffer.buffer;
+    let storage = buffer.buffer;
     const total = buffered + provided;
     if (total > storage.byteLength) {
-      const storage = new ArrayBuffer(bitCeil(total));
-      const realloc = new Uint8Array(storage, 0, total);
-      realloc.set(buffer);
-      this.#buffer = realloc;
-    } else {
-      this.#buffer = new Uint8Array(storage, 0, total);
+      storage = storage.transfer(max(total, storage.byteLength * 2));
     }
+    this.#buffer = new Uint8Array(storage, 0, total);
     this.#buffer.set(chunk, buffered);
   }
 
